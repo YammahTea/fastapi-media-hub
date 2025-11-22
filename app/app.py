d1afde67121cc25e2f456123416e79ec78d529bc
clocky <clockyTestEmail@gmail.com>
2025-11-22 20:46:10 +0300

    Added a test end point

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/our_end")
-def our_end():
+@app.get("/hello-world")
+def hello_world():
     return {"message": "hello!"}
     #Why do we return a dict? because when we create APIs, we deal with something called JSON (Javascript Object Notation)
