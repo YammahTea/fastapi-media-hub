4c19457fe376a68cb7f393be80fa2b55785c9472
clocky <clockyTestEmail@gmail.com>
2025-11-23 18:21:10 +0300

    Fixed naming conflict with the database

--- app/schemas.py
+++ app/schemas.py
@@ -1,14 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from fastapi_users import schemas
 import uuid
+from datetime import datetime
 
-class PostCreate(BaseModel):
-    title: str
-    description: str
+# --- Post Schemas ---
+class PostBase(BaseModel):
+    caption: str
 
-class PostResponse(BaseModel):
-    title: str
-    description: str
+class PostCreate(PostBase):
+    pass
+
+class PostResponse(PostBase):
+    id: uuid.UUID
+    user_id: uuid.UUID
+    url: str
+    file_type: str
+    file_name: str
+    created_at: datetime
+    email: str | None = None
+
+    model_config = ConfigDict(from_attributes=True)
+
+# --- User Schemas ---
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
     pass
